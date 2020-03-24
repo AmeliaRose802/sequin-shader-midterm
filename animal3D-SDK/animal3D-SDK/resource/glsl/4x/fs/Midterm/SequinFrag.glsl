b0f734cf7dd52be40a38e85bf8a9f7fa79034e37
Amelia <amelia.payne@mymail.champlain.edu>
2020-03-24 14:13:11 -0400

    Sequines now have offset to draw hex pattern

--- animal3D-SDK/animal3D-SDK/resource/glsl/4x/fs/Midterm/SequinFrag.glsl
+++ animal3D-SDK/animal3D-SDK/resource/glsl/4x/fs/Midterm/SequinFrag.glsl
@@ -22,11 +22,48 @@ vec2 getCenterOffset(){
 }
 
 vec2 getRemappedCenter(vec2 currentUV){
+    
+    //Bucket y 
+    float newY = currentUV.y * numSequines.y;
+    newY = ceil(newY);
+    newY = newY / numSequines.y;
+    
+    
+    
+    
+    //Do stuff for x based on result of y
+    
+    /*
     vec2 newUV = currentUV * numSequines; //.6 becomes 1.2 
     newUV = ceil(newUV); // 1.2 becomes 2
     newUV = newUV / numSequines; // 2 becomes 1
+   */
     
-    return newUV - getCenterOffset();
+    float newX = currentUV.x * numSequines.x;
+    newX = ceil(newX);
+    newX = newX / numSequines.x;
+    
+    
+    //Get every other row of sequines 
+    float test = newY * numSequines.y;
+    if(mod(test, 2.0) != 0.0 ){
+        
+        
+        
+        //Bucket the output for the second row. 
+        //.4 becomes . 75 
+        //.6 becomes .75 
+        //1.2 becomes 1.25
+        //.8 becomes 1.25
+        float k = 1.0 / (numSequines.x * 2.0);
+        float offsetX = (ceil((currentUV.x + k) * numSequines.x) / numSequines.x) - k;
+        
+        
+        newX = offsetX;
+    }
+    
+    
+    return vec2(newX, newY) - getCenterOffset();
 }
 
 
